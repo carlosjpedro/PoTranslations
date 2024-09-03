6613bdc909e262bf04cec18f221681bada85149d
carlo <carlo@DESKTOP-T3HA0FB>
2024-09-03 15:51:43 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var fileReader = new FileReader();
+            var contents = fileReader.ReadFileAsStrings("C:\\Users\\carlo\\Downloads\\sv.po");
+
+            await foreach (var line in contents)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+    public interface IFileReader
+    {
+        IAsyncEnumerable<string?> ReadFileAsStrings(string filePath);
+    }
+
+    public class FileReader : IFileReader
+    {
+        public async IAsyncEnumerable<string?> ReadFileAsStrings(string filePath)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(filePath);
+
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException("File is not a present or is accessible", nameof(filePath));
+            }
+
+            await using var fileStream = File.OpenRead(filePath);
+            using var streamReader = new StreamReader(fileStream);
+
+            while (!streamReader.EndOfStream)
+            {
+                yield return await streamReader.ReadLineAsync();
+            }
+        }
+    }
+}
